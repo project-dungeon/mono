ac9b4a194d986068a42b47b90f808c754d9c41e0
k-koehler <k.koehler.dev@gmail.com>
2023-11-28 09:46:34 -0600

    fps

--- client/src/controllers/debug-info.controller.js
+++ client/src/controllers/debug-info.controller.js
@@ -3,6 +3,9 @@ import Controller from "./controller";
 
 export default class DebugController extends Controller {
   static #debugInfo;
+  static #lastCalledTime;
+  static #fps;
+
   static {
     DebugController.#debugInfo = document.getElementById("debug-info");
     DebugController.#debugInfo.style.display = "block";
@@ -12,18 +15,27 @@ export default class DebugController extends Controller {
     if (!player) {
       return;
     }
+    if (!DebugController.#lastCalledTime) {
+      DebugController.#lastCalledTime = Date.now();
+      DebugController.#fps = 0;
+      return;
+    }
+    const delta = (Date.now() - DebugController.#lastCalledTime) / 1000;
+    DebugController.#lastCalledTime = Date.now();
+    DebugController.#fps = 1 / delta;
     DebugController.#debugInfo.innerHTML = `
-      <p>Player id: ${player.id}</p>
-      <p>Player position: ${player.gameObject.position.x.toFixed(
+      <p class="debug-info-p">Player id: ${player.id}</p>
+      <p class="debug-info-p">Player position: ${player.gameObject.position.x.toFixed(
         1
       )}, ${player.gameObject.position.z.toFixed(1)}</p>
       ${
         player.serverPosition
-          ? `<p>Server position: ${player.serverPosition.x.toFixed(
+          ? ` <p class="debug-info-p">Server position: ${player.serverPosition.x.toFixed(
               1
             )}, ${player.serverPosition.y.toFixed(1)}</p>`
           : ""
       }
+       <p class="debug-info-p">FPS: ${DebugController.#fps.toFixed(1)}</p>
     `;
   }
 }
